3444deeea421e7618b5fa583812a094caae71df7
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-26 15:38:40 -0500

    Scope in models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ApplicationRecord
+
+  scope :finished, ->{ where.not(finished_on: nil) }
+  scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
+
+  # def self.recent
+  #   where('finished_on > ?', 2.days.ago)
+  # end
+
   def finished?
     finished_on.present?
   end
